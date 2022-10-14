48142806f788edaf9985722ab55552cac2a96fee
jackmcnamarauq <101162678+jackmcnamarauq@users.noreply.github.com>
2022-10-14 12:31:13 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     author_email = 'maze@pyth0n.org',
     url          = 'https://maze.io/',
     description  = ('Modem implementations for XMODEM, YMODEM and ZMODEM'),
-    long_description = file('doc/source/about.rst').read(),
     license      = 'MIT',
     keywords     = 'xmodem ymodem zmodem protocol',
     packages     = ['modem'],
